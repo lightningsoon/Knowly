66cf08541e59734e4d8cd2c22c1033567b6920a1
18092125052 <lightingsoon@163.com>
2025-05-20 00:33:17 +0800

    v1

--- gui.py
+++ gui.py
@@ -44,8 +44,7 @@ def get_doc_page():
         except Exception as e:
             print("获取文档列表失败",e)
             return []
-    # def refresh_list():
-    #     return random.sample(['1','2','3'],2)
+
     def delete_file(selected_doc):
         try:
             if not selected_doc:
@@ -86,28 +85,27 @@ def get_doc_page():
         except Exception as e:
             return f"检索失败: {str(e)}"
             
-    def gr_refresh_list():
-        return gr.update(choices=refresh_list())
 
-    with gr.Blocks(title="文档管理系统") as doc_page:
-        gr.Markdown("# 文档管理系统")
+
+    with gr.Blocks(title="知了——知识库管理系统") as doc_page:
+        gr.Markdown("# 知了——知识库管理系统\n 此处应有一些介绍。")
         
         # 文档管理部分
-        gr.Markdown("## 文档管理")
+        gr.Markdown("## 上传")
         with gr.Group():
             file_input = gr.File(label="上传文档")
             with gr.Row():
                 upload_btn = gr.Button("上传")
                 process_btn = gr.Button("处理")
             upload_output = gr.Textbox(label="操作结果")
-        
+        gr.Markdown("## 删除")
         with gr.Group():
             doc_dropdown = gr.Dropdown(
                 info="选择要处理的文档",
-                choices=[],
+                choices=refresh_list(),
                 label="文档列表",
                 multiselect=False,
-                allow_custom_value=True
+                allow_custom_value=True,interactive=True
             )
             refresh_btn = gr.Button("刷新列表")
             delete_btn = gr.Button("删除选中文档")
@@ -125,11 +123,9 @@ def get_doc_page():
         # 绑定事件
         upload_btn.click(upload_file, inputs=[file_input], outputs=[upload_output])
         process_btn.click(process_file, inputs=[doc_dropdown], outputs=[upload_output])
-        refresh_btn.click(gr_refresh_list,outputs=[doc_dropdown])
+        refresh_btn.click(fn=lambda :gr.update(choices=refresh_list()),outputs=[doc_dropdown])
         delete_btn.click(delete_file, inputs=[doc_dropdown], outputs=[delete_output])
         search_btn.click(search_docs, inputs=[search_input], outputs=[search_output])
         
-        # 初始加载文档列表 (传入 request)
-        doc_page.load(refresh_list, inputs=None, outputs=[doc_dropdown])
-    
+        doc_page.load(fn=lambda :gr.update(choices=refresh_list()),inputs=[],outputs=[doc_dropdown])
     return doc_page 
